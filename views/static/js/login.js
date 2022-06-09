3bfc3f416722459bc2ec5bbb65b3a626b70cff94
Leonid Prokopev <leo.messi32@yandex.ru>
2022-06-09 20:28:01 +0300

    change stylesheets

--- views/static/js/login.js
+++ views/static/js/login.js
@@ -1,6 +1,5 @@
 async function login() {
     var type = $.cookie("type");
-    console.log(type);
     var pass = document.getElementById("password").value;
     var email = document.getElementById("email").value;
     var response = await fetch('/token/' + type, {
