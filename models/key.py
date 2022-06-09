5fa779a2e7a0e4c526fe8ad63dde43d59c71beaa
Leonid Prokopev <leo.messi32@yandex.ru>
2022-06-09 10:35:31 +0300

    added key model

---
+++ models/key.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+from pydantic import BaseModel
+from data.data import Base
+
+
+class Key(Base):
+    id = Column(Integer, primary_key=True)
+    value = Column(String)
+    name = Column(String)
+    surname = Column(String)
+    group = Column(String)
+
+
+class ApiKey(BaseModel):
+    value: str
+    name: str
+    surname: str
+    group: str
